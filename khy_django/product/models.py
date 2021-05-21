85f81099de58e734698eb7cc886ce18d9a8db2af
khy0425 <from_ap@naver.com>
2021-05-21 23:21:58 +0900

    Model 구성

--- khy_django/product/models.py
+++ khy_django/product/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=256, verbose_name = '상품명')
+    price = models.IntegerField(verbose_name = '상품가격')
+    description = models.TextField(verbose_name = '상품설명')
+    stuck = models.IntegerField(verbose_name ='재고')
+    register_date = models.DateTimeFieldField(auto_now_add=True, verbose_name='등록날짜')
+
+    class Meta:
+        db_table = 'khy_order'
+        verbose_name = '상품'
+        verbose_name_plural = '상품'
