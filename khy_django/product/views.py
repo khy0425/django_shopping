0ad09bbf7c33691dd46cbed3d28394d9570e0b5b
khy0425 <from_ap@naver.com>
2021-05-31 00:20:51 +0900

    재고 등록

--- khy_django/product/views.py
+++ khy_django/product/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.views.generic.edit import FormView
 from .models import Product
+from .forms import RegisterForm
 # Create your views here.
 
 
@@ -8,3 +10,13 @@ class ProductList(ListView):
     model = Product
     template_name = 'product.html'
     context_object_name = 'product_list'
+
+
+class ProductCreate(FormView):
+    model = Product
+    template_name = 'product.html'
+    context_object_name = 'product_list'
+
+    template_name = 'register_product.html'
+    form_class = RegisterForm
+    success_url = '/product/'
