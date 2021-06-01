c5b23d1c5ad5711482f99e9fa486cba33959be4e
khy0425 <from_ap@naver.com>
2021-06-01 21:16:25 +0900

    상품창에서 자세히보기 링크 및 html 활성화

--- khy_django/product/views.py
+++ khy_django/product/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.views.generic.edit import FormView
 from .models import Product
 from .forms import RegisterForm
@@ -20,3 +20,8 @@ class ProductCreate(FormView):
     template_name = 'register_product.html'
     form_class = RegisterForm
     success_url = '/product/'
+
+
+class ProductDetail(DetailView):
+    template_name = 'product_detail.html'
+    queryset = Product.objects.all()
