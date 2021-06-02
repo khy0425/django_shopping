176c3a53741c0e067ecedd2f1ebf2a8f83bd0e73
khy0425 <from_ap@naver.com>
2021-06-02 18:15:27 +0900

    상세페이지에서 주문넣기

--- khy_django/product/views.py
+++ khy_django/product/views.py
@@ -3,6 +3,7 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.edit import FormView
 from .models import Product
 from .forms import RegisterForm
+from order.forms import RegisterForm as OrderForm
 # Create your views here.
 
 
@@ -25,3 +26,9 @@ class ProductCreate(FormView):
 class ProductDetail(DetailView):
     template_name = 'product_detail.html'
     queryset = Product.objects.all()
+    context_object_name = 'product'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = OrderForm()
+        return context
